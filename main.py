89186d3f052f6c5b00b4961690b5a5d87edb7c15
John Allison <john123allison@gmail.com>
2021-01-16 00:27:04 -0500

    adding token and config loading

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import discord
 import requests
-
+import json
 
 client = discord.Client()
 
+def load_config(filename: str) -> str:
+    with open(filename, "r") as config:
+        data = json.load(config)
+        token = data["token"]
+        return token
 
 class HackpackBot(discord.Client):
     """
     The main bot class
     """
+    prefix = '!'
+
     async def on_ready(self):
         print("Hello, {0}!".format(self.user))
 
     @client.event
-    async def on_message(message: discord.Message):
-        if message.author != client.user and message.content.startswith('$hello'):
+    async def on_message(self, message: discord.Message):
+        if message.author != client.user and message.content.startswith(self.prefix + 'hello'):
             await message.channel.send("Hello!")
     
     def get_ctf_upcoming(self, number: int, start_timestamp: int, end_timestamp: int):
@@ -28,9 +35,11 @@ class HackpackBot(discord.Client):
 
 
 def main():
+    token = load_config("config.json")
+
     client = HackpackBot()
     client.get_ctf_upcoming(100, 1422019499, 1423029499)
-    client.run('token goes here')
+    client.run(token)
 
 
 if __name__ == '__main__':
