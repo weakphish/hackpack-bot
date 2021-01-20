323e0abd6fe1ab48bbe3fe19259d865eea98701e
John Allison <john123allison@gmail.com>
2021-01-19 20:38:53 -0500

    Create CTF channels for ctfs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.channel import CategoryChannel
 import requests
 import json
 
@@ -31,13 +32,31 @@ class HackpackBot(discord.Client):
         """
         if message.author == client.user:
             return
+        # !hello - Says hello!
         if message.content.startswith(self.prefix + 'hello'):
             await message.channel.send("Hello!")
+        # !ctf list - Lists upcoming CTFs from CTFtime
         if message.content.startswith(self.prefix + 'ctf') and "list" in message.content:
             await message.channel.send("Getting upcoming CTFs...")
             ctfs_upcoming = self.get_ctf_upcoming(5)
             for embed_var in ctfs_upcoming:
                 await message.channel.send(embed=embed_var)
+        # !ctf create ____ - Creates a new CTF event for the server with a role and channel
+        elif message.content.startswith(self.prefix + 'ctf') and "create" in message.content:
+            # This is a bit spaghetti, I am so sorry - John
+            ctf_name = message.content.split(" ")[2]
+            guild = self.guilds[0]
+            ctf_role = await guild.create_role(name=ctf_name)
+            overwrites = {
+                guild.default_role: discord.PermissionOverwrite(read_messages=False),
+                guild.get_role(ctf_role.id): discord.PermissionOverwrite(read_messages=True)
+            }
+            ctf_category_name = "CTFs"
+            category = discord.utils.get(
+                guild.categories, name=ctf_category_name)
+            await guild.create_text_channel(name=ctf_name, overwrites=overwrites, category=category)
+        # TODO !ctf join   ____ - Join the given CTF role / channel
+        # TODO !ctf delete ____ - Remove CTF role and channel
 
     @client.event
     async def on_member_join(self, member):
