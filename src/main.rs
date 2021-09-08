1f2077c009aae421b1e99024db4639cc2c20d49c
John Allison <john123allison@gmail.com>
2021-09-08 13:25:17 -0400

    bot can run

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 use std::env;
 
-use serenity::client::Client;
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+#[group]
+struct General;
 
 #[tokio::main]
 async fn main() {
+    // Load framework
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!"))
+        .group(&GENERAL_GROUP);
+
     // Load token into client
     let token = env::var("DISCORD_TOKEN").expect("Token error"); 
-    let mut client = Client::builder(token).await.expect("Could not create client."); 
+    let mut client = Client::builder(token)
+        .framework(framework)
+        .await
+        .expect("Could not create client."); 
 
     // Start listening on the client (single shard)
     if let Err(why) = client.start().await {
