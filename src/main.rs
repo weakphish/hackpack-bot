56316aa14dd1f7a1896a1aaca29ba96609afdef3
John Allison <john123allison@gmail.com>
2021-09-08 12:53:06 -0400

    update main function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::env;
 
-use serenity::Client::Client;
+use serenity::client::Client;
 
 #[tokio::main]
 async fn main() {
     // Load token into client
-    let token = env::var("DISCORD_TOKEN").expect("Token error."); 
+    let token = env::var("DISCORD_TOKEN").expect("Token error"); 
     let mut client = Client::builder(token).await.expect("Could not create client."); 
 
     // Start listening on the client (single shard)
     if let Err(why) = client.start().await {
-        println!("An error occurred starting the clinet: {:?}", why);
+        println!("An error occurred starting the client: {:?}", why);
     }
 }
