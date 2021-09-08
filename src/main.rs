c6be774a0a49264b4574040cfdba1c539250a9b4
John Allison <john123allison@gmail.com>
2021-09-08 12:48:54 -0400

    skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serenity::Client::Client;
+
+#[tokio::main]
+async fn main() {
+    // Load token into client
+    let token = env::var("DISCORD_TOKEN").expect("Token error."); 
+    let mut client = Client::builder(token).await.expect("Could not create client."); 
+
+    // Start listening on the client (single shard)
+    if let Err(why) = client.start().await {
+        println!("An error occurred starting the clinet: {:?}", why);
+    }
 }
