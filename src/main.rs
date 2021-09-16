c406d39c910f6d513ee18d7408718ba29df56e59
John Allison <john123allison@gmail.com>
2021-09-16 15:23:05 -0400

    model for working slash command in place

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::fs;
 
+use serenity::model::id::GuildId;
 use serenity::{Client, async_trait};
 use serenity::client::{Context, EventHandler};
 use serenity::framework::standard::{macros::group, StandardFramework};
 use serenity::model::gateway::Ready;
 use serenity::model::interactions::application_command::ApplicationCommand;
-use serenity::model::interactions::Interaction;
+use serenity::model::interactions::{Interaction, InteractionResponseType};
 
 #[group]
 struct General;
@@ -17,7 +18,7 @@ impl EventHandler for Handler {
     // From docs:
     // Dispatched upon startup.
     // Provides data about the bot and the guilds it’s in.
-    async fn ready(&self, ctx: Context, data_about_bot: Ready) {
+    async fn ready(&self, ctx: Context, _data_about_bot: Ready) {
         let commands = ApplicationCommand::set_global_application_commands(&ctx.http, |commands| {
             commands.create_application_command(|command| {
                 command.name("ping").description("A ping command")
@@ -28,17 +29,34 @@ impl EventHandler for Handler {
             "I now have the following global slash commands: {:#?}",
             commands
         );
+
+        let guild_command = GuildId(123456789)
+            .create_application_command(&ctx.http, |command| {
+                command.name("ping_guild").description("A guild-specific ping command")
+            })
+            .await;
+
+        println!("I created the following guild command: {:#?}", guild_command);
     }
 
     // From docs:
     // Dispatched when an interaction is created (e.g a slash command was used or a button was clicked).
     // Provides the created interaction.
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+    async fn interaction_create(&self, _ctx: Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
-            let content = match command.data.name.as_str() {
+            let _content = match command.data.name.as_str() {
                 "ping" => "Hey!".to_string(),
                 _ => "I didn't get that.".to_string(),
             };
+            if let Err(why) = command
+                    .create_interaction_response(&_ctx.http, |response| {
+                        response
+                            .kind(InteractionResponseType::ChannelMessageWithSource)
+                            .interaction_response_data(|message| message.content(_content))
+                    }).await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
         }
     }
 }
