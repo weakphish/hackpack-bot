5bacb00aae5bd67cb10d7d6be30b93a11b1b357c
John Allison <john123allison@gmail.com>
2021-12-17 14:40:57 -0500

    lord forgive me for we have rewritten once more

---
+++ commands.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/bwmarrin/discordgo"
+
+// Define our commands
+var (
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "test-command",
+			Description: "Basic command.",
+		},
+	}
+
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"basic-command": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Hey there! Congratulations, you just executed your first slash command",
+				},
+			})
+		},
+	}
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// Command line flags
+var (
+	GuildID        = flag.String("guild", "", "Test")
+	BotToken       = flag.String("token", "", "Bot access token")
+	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdown")
+)
+
+// Session pointer for our discord session
+var session *discordgo.Session
+
+// Parse the cmd flags
+func init() {
+	flag.Parse()
+}
+
+// Init our bot session
+func init() {
+	var err error
+	session, err = discordgo.New("Bot " + *BotToken)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+}
+
+// Add handlers
+func init() {
+	session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+}
+
+func main() {
+	session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Println("Bot is up!")
+	})
+	err := session.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+
+	for _, v := range commands {
+		_, err := session.ApplicationCommandCreate(session.State.User.ID, *GuildID, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+	}
+
+	defer session.Close()
+
+	stop := make(chan os.Signal)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	log.Println("Gracefully shutting down")
+}
