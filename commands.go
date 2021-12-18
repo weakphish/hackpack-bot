9c9b2fcba3464f32cb38c2afd9598b30253ce39e
John Allison <john123allison@gmail.com>
2021-12-17 22:32:24 -0500

    slash command skeleton

--- commands.go
+++ commands.go
@@ -6,17 +6,68 @@ import "github.com/bwmarrin/discordgo"
 var (
 	commands = []*discordgo.ApplicationCommand{
 		{
-			Name:        "test-command",
-			Description: "Basic command.",
+			Name:        "ping",
+			Description: "ping-command",
+		},
+		{
+			Name:        "ctf",
+			Type:        discordgo.ChatApplicationCommand,
+			Description: "Parent command for the CTF group",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:        "create",
+					Type:        discordgo.ApplicationCommandOptionSubCommand,
+					Description: "Create a CTF",
+				},
+				{
+					Name:        "join",
+					Type:        discordgo.ApplicationCommandOptionSubCommand,
+					Description: "Join a CTF",
+				},
+			},
 		},
 	}
 
+	// Create a map of <CommandName>:<HandlerFunction> for each command. Each command will
+	// correspond to a first-class function that will handle the command's usage.
 	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-		"basic-command": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		// Ping command
+		"ping": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
 					Content: "Hey there! Congratulations, you just executed your first slash command",
+				},
+			})
+		},
+		// CTF Group
+		// Reference: https://github.com/bwmarrin/discordgo/blob/master/examples/slash_commands/main.go
+		// Line 227
+		"ctf": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			var content string
+
+			// As you can see, the name of subcommand (nested, top-level) or subcommand group
+			// is provided through arguments.
+			switch i.ApplicationCommandData().Options[0].Name {
+			case "subcmd":
+				content = "The top-level subcommand is executed. Now try to execute the nested one."
+			default:
+				if i.ApplicationCommandData().Options[0].Name != "scmd-grp" {
+					return
+				}
+				switch i.ApplicationCommandData().Options[0].Options[0].Name {
+				case "nst-subcmd":
+					content = "Nice, now you know how to execute nested commands too"
+				default:
+					// I added this in the case something might go wrong
+					content = "Oops, something gone wrong.\n" +
+						"Hol' up, you aren't supposed to see this message."
+				}
+			}
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: content,
 				},
 			})
 		},
